05699dd7cd689a95634862fb08b29d826913f3c7
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-05 21:16:35 +0000

    add git signs

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,9 +1,12 @@
 return { {
-	'echasnovski/mini.nvim',
-	version = false,
-	-- enabled=false,
-	config = function()
-		local statusline = require 'mini.statusline'
-		statusline.setup { use_icons = true }
-	end
+  'echasnovski/mini.nvim',
+  version = false,
+  -- enabled=false,
+  config = function()
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = true }
+
+    -- not sure how to set this up
+    -- require('mini.git').setup({})
+  end
 } }
