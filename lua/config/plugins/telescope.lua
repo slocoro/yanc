8557020c13357f3bbfde4709d86d552b5522a3c1
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-06 09:44:33 +0000

    add telescope for keymaps

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -31,6 +31,7 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope key maps' })
 
       vim.keymap.set('n', '<leader>en', function()
           -- local opts = require('telescope.themes').get_ivy({
