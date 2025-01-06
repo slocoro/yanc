774b71821dcbb488136f083139450b18dbc954e5
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-06 09:43:55 +0000

    change git config

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -20,7 +20,7 @@ return {
       },
       signs_staged_enable          = true,
       signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+      numhl                        = true,  -- Toggle with `:Gitsigns toggle_numhl`
       linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir                 = {
@@ -28,7 +28,7 @@ return {
       },
       auto_attach                  = true,
       attach_to_untracked          = false,
-      current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts      = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
