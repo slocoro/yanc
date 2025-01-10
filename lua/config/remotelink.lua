bce113f31d2f23c98aa4f1d3f492afa72cc6c2de
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-10 11:20:47 +0000

    add minimal error handling to show useful message when use RemoteLink in non git repo, add type annotations

--- lua/config/remotelink.lua
+++ lua/config/remotelink.lua
@@ -1,6 +1,5 @@
 -- TODO:
 -- add error handling and testing
--- add annotations
 -- make config handling nicer and allow passing of config through setup (config should be merged with defaults)
 
 -- convention
@@ -23,10 +22,14 @@ M.opts = {
   },
 }
 
+---@param cmd table
+---@return string
 local run_command = function(cmd)
   return vim.fn.system(cmd)
 end
 
+---@param pattern string
+---@return string
 local escape_magic = function(pattern)
   -- this escapes magic characters (^$()%.[]*+-?) so the string can be used in a pattern
   -- (e.g. :match, :gsub, ...)
@@ -34,20 +37,24 @@ local escape_magic = function(pattern)
   return (pattern:gsub("%W", "%%%1"))
 end
 
+---@return string
 local get_sha = function()
   return run_command({ "git", "rev-parse", "HEAD" })
 end
 
+---@return string
 local get_git_root = function()
   -- removes the \n character at the end
   return run_command({ "git", "rev-parse", "--show-toplevel" }):match("[^/]+$"):gsub("\n", "")
 end
 
+---@return string
 local get_filepath = function()
   -- full file path
   return vim.fn.expand("%:p")
 end
 
+---@return table
 local get_repo_info = function()
   local git_url = run_command({ "git", "remote", "get-url", "origin" }):gsub("\n", "")
   -- parse url
@@ -67,6 +74,9 @@ local get_repo_info = function()
   return { nil, nil, nil }
 end
 
+---@param filepath string
+---@param git_root string
+---@return string
 local get_filename = function(filepath, git_root)
   -- Derives filename from filepath and git root. removes from final path as root
   -- can be different than the repo name (e.g. git clone into a specific dir)
@@ -74,7 +84,23 @@ local get_filename = function(filepath, git_root)
   return filepath:match(".*(" .. escape_magic(git_root) .. ".*)"):gsub("^" .. escape_magic(git_root) .. "/", "")
 end
 
+---@return boolean
+local is_git_repo = function()
+  local git_status = run_command({ "git", "status" })
+  if git_status:match("fatal: not a git repository") then
+    return false
+  end
+  return true
+end
+
+---@return string|nil
 M.get_remotelink = function()
+  -- validate if git repo and return if not
+  if not is_git_repo() then
+    print("WARNING: RemoteLink only works in git repos")
+    return
+  end
+
   local line_number = vim.fn.line(".")
   local protocol = "https://"
   -- this could be controlled by env variable (or setting from setup)
@@ -124,6 +150,9 @@ M.setup = function()
   -- Define command
   vim.api.nvim_create_user_command("RemoteLink", function()
     local remotelink = M.get_remotelink()
+    if not remotelink then
+      return
+    end
     -- copy variable to unnamedplus register
     vim.fn.setreg("+", remotelink)
     vim.print("Created remote link: " .. remotelink)
