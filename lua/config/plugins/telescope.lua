849a283db5e26db7775312cb52bd939f673ee11a
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-10 13:50:28 +0000

    search hidden files in telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -51,7 +51,10 @@ return {
 
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", function()
-        builtin.find_files({ hidden = true })
+        builtin.find_files({
+          -- hidden = true,
+          find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+        })
       end, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
