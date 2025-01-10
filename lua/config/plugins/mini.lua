ab3c012c2ba9dc493d9e050987043b0ae1e3ae8e
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-10 13:51:11 +0000

    enable mini pairs

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,12 +1,15 @@
-return { {
-  'echasnovski/mini.nvim',
-  version = false,
-  -- enabled=false,
-  config = function()
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = true }
+return {
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    -- enabled=false,
+    config = function()
+      local statusline = require("mini.statusline")
+      statusline.setup({ use_icons = true })
 
-    -- not sure how to set this up
-    -- require('mini.git').setup({})
-  end
-} }
+      require("mini.pairs").setup({})
+      -- not sure how to set this up
+      -- require('mini.git').setup({})
+    end,
+  },
+}
