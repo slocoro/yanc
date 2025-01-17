8c6344698e5e902f5f485f46e071d6dd20e86c17
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-17 13:25:14 +0000

    modify to have same format as mlp monorepo

--- lua/config/plugins/formatting.lua
+++ lua/config/plugins/formatting.lua
@@ -1,18 +1,29 @@
 return {
-  'stevearc/conform.nvim',
+  "stevearc/conform.nvim",
+  -- TODO: figure how to load this at a better time e.g. before saving
   lazy = false,
   opts = {
+    log_level = vim.log.levels.DEBUG,
     notify_on_error = false,
     format_on_save = true,
     formatters_by_ft = {
       -- go = { 'gofmt' },
       -- rust = { 'rustfmt' },
-      python = { 'black' },
-      c = { 'clang_format' },
-      lua = { 'stylua' },
-      bash = { 'shfmt', 'shellcheck' },
-      zsh = { 'shfmt', 'shellcheck' },
-      sh = { 'shfmt', 'shellcheck' },
+      python = { "black" },
+      c = { "clang_format" },
+      lua = { "stylua" },
+      bash = { "shfmt", "shellcheck" },
+      zsh = { "shfmt", "shellcheck" },
+      sh = { "shfmt", "shellcheck" },
+    },
+    formatters = {
+      shfmt = {
+        -- couldn't get this to work with pre/append and inherit=true
+        -- command had -i 4 at the end which caused the indentation to be 4
+        inherit = false,
+        command = "shfmt",
+        args = { "-i", "2", "-ci", "-sr", "-filename", "$FILENAME" },
+      },
     },
   },
 }
