9e52bd943ca55de05ffe5aed764e4cb4561ff356
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-01-22 10:33:15 +0000

    update todo

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 -- TODO:
 -- add session handling
 -- improve time to open files (quite slow to open first py at the moment, subsequent ones are ok, could be because first file starts lsp?)
--- add keymaps to close buffers in telescope
 
 -- test function
 -- can be run using :lua MyTestFunc()
