ceb651a8aebdef7e2e990751184576672e9e2b9c
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-03-09 21:09:28 +0000

    add autocommand to show diagnostic hover

--- init.lua
+++ init.lua
@@ -16,6 +16,30 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- show diagnostics for symbol under cursor
+-- https://neovim.discourse.group/t/how-to-show-diagnostics-on-hover/3830/5
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  pattern = "*",
+  callback = function()
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_get_config(winid).zindex then
+        return
+      end
+    end
+    vim.diagnostic.open_float({
+      scope = "cursor",
+      focusable = false,
+      close_events = {
+        "CursorMoved",
+        "CursorMovedI",
+        "BufHidden",
+        "InsertCharPre",
+        "WinLeave",
+      },
+    })
+  end,
+})
+
 -- highlighting of cursor using lsp
 vim.api.nvim_create_autocmd("CursorHold", {
   pattern = "*",
