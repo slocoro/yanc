a58cdf4426c42ed6d8653b9c934ed7376525575d
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-03-17 22:16:50 +0000

    modify tab width for tf files

--- after/ftplugin/terraform.lua
+++ after/ftplugin/terraform.lua
@@ -1,3 +1,8 @@
+-- options dedicated to .tf files
+local set = vim.opt_local
+
+set.shiftwidth = 2
+
 -- https://neovim.discourse.group/t/commentstring-for-terraform-files-not-set/4066
 -- fixes "gcc" (commenting code) not working for terraform because "commentstring" is not set
 vim.bo.commentstring = "# %s"
