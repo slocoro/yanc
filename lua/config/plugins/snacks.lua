1a5efe3d30f6c0a3b5c93ccb0f7887f315aa48aa
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-03-23 11:18:38 +0000

    modify backdrop for lazygit floating window

--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -8,7 +8,13 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     -- scroll = { enabled = true },
-    lazygit = { configure = false },
+    lazygit = {
+      configure = false,
+      win = {
+        -- controls how dim the backdrop becomes when pop up opens
+        backdrop = 100,
+      },
+    },
   },
   keys = {
     {
