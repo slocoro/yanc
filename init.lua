de4a1271d9ec2618bc1f0e0dae4e8baff13f2333
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-03-23 11:54:31 +0000

    remove comment

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
--- TODO:
--- improve time to open files (quite slow to open first py at the moment, subsequent ones are ok, could be because first file starts lsp?)
-
 -- this makes Ghostty show the working dir as tab name
 if vim.fn.getenv("TERM_PROGRAM") == "ghostty" then
   vim.opt.title = true
