a832c7993a330d2cfc1efdf311ffd919bf1dddb6
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-03-23 11:54:16 +0000

    move color schemes into own file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,78 +52,10 @@ require("lazy").setup({
     notify = false, -- get a notification when changes are found
   },
   spec = {
-    {
-      "folke/tokyonight.nvim",
-      config = function()
-        vim.cmd.colorscheme("tokyonight-moon")
-
-        local opts = { noremap = true, silent = true }
-        vim.keymap.set(
-          "n",
-          "<leader>tm",
-          ToggleDarkMode,
-          vim.tbl_extend("force", opts, { desc = "[t]oggle dark[m]ode" })
-        )
-      end,
-      lazy = false,
-      priority = 1000,
-    },
-    -- lua/plugins/rose-pine.lua
-    -- {
-    --   "rose-pine/neovim",
-    --   name = "rose-pine",
-    --   config = function()
-    --     require("rose-pine").setup({
-    --       variant = "dawn", -- auto, main, moon, or dawn
-    --     })
-    --     vim.cmd("colorscheme rose-pine")
-    --   end,
-    --   lazy = false,
-    --   priority = 1000,
-    -- },
-    -- {
-    --   "rebelot/kanagawa.nvim",
-    --   config = function()
-    --     vim.cmd.colorscheme("kanagawa-wave")
-    --   end,
-    --   lazy = false,
-    --   priority = 1000,
-    -- },
-    -- {
-    --   "navarasu/onedark.nvim",
-    --   config = function()
-    --     require("onedark").setup({ style = "dark" })
-    --     vim.cmd.colorscheme("onedark")
-    --   end,
-    --   lazy = false,
-    --   priority = 1000,
-    -- },
-    -- {
-    --   'sainnhe/gruvbox-material',
-    --   lazy = false,
-    --   priority = 1000,
-    --   config = function()
-    --     -- Optionally configure and load the colorscheme
-    --     -- directly inside the plugin declaration.
-    --     -- (remember, lazy "installs" the plugin (i.e. downloads the code
-    --     -- it doesn't actually set the color theme which is why it's set
-    --     -- in the config function)
-    --     vim.g.gruvbox_material_enable_italic = true
-    --     vim.cmd.colorscheme('gruvbox-material')
-    --   end
-    -- },
-    -- {
-    --   "catppuccin/nvim",
-    --   lazy = false,
-    --   name = "catppuccin",
-    --   priority = 1000,
-    --   config = function()
-    --     vim.cmd.colorscheme('catppuccin-frappe')
-    --   end
-    -- },
     -- import your plugins
     -- this needs to match the folder structure you use to organise
     -- your plugins, in this case lua/congig/plugins
+    { import = "config.themes" },
     { import = "config.plugins" },
   },
 })
