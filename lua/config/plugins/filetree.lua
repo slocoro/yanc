c240276fa62cce0b6c803384c91062472e01bf75
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-03-25 23:47:48 +0000

    add proper handling of hidden files in filetree

--- lua/config/plugins/filetree.lua
+++ lua/config/plugins/filetree.lua
@@ -9,6 +9,17 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     event = "VeryLazy",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_by_name = {
+            ".git",
+            ".DS_store",
+          },
+        },
+      },
+    },
     keys = {
       { "<leader>E", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
       { "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
