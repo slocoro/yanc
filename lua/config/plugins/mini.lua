c3bfe8712113fc326b9834eb69db430f1a6ec283
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-04-27 10:28:44 +0100

    remove top level table

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,16 +1,15 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    version = false,
-    -- enabled=false,
-    config = function()
-      -- local statusline = require("mini.statusline")
-      -- statusline.setup({ use_icons = true })
+  "echasnovski/mini.nvim",
+  version = false,
+  -- enabled=false,
+  config = function()
+    -- local statusline = require("mini.statusline")
+    -- statusline.setup({ use_icons = true })
 
-      require("mini.pairs").setup({})
-      require("mini.surround").setup({})
+    require("mini.pairs").setup({})
+    require("mini.surround").setup({})
+    require("mini.files").setup({})
 
-      -- require('mini.git').setup({})
-    end,
-  },
+    -- require('mini.git').setup({})
+  end,
 }
