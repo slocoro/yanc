65fb985e730f7b6277c556e2b223b75a99d6f853
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-04-29 19:58:41 +0100

    add code to test mini.file but keep neotree enabled

--- lua/config/plugins/filetree.lua
+++ lua/config/plugins/filetree.lua
@@ -1,28 +1,27 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    event = "VeryLazy",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_by_name = {
-            ".git",
-            ".DS_store",
-          },
+  "nvim-neo-tree/neo-tree.nvim",
+  enabled = true,
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  event = "VeryLazy",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+          ".DS_store",
         },
       },
     },
-    keys = {
-      { "<leader>E", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
-      { "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
-    },
+  },
+  keys = {
+    { "<leader>E", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
+    { "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
   },
 }
--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,15 +1,24 @@
 return {
   "echasnovski/mini.nvim",
   version = false,
-  -- enabled=false,
+  enabled = true,
   config = function()
     -- local statusline = require("mini.statusline")
     -- statusline.setup({ use_icons = true })
 
     require("mini.pairs").setup({})
     require("mini.surround").setup({})
-    require("mini.files").setup({})
-
+    -- require("mini.files").setup({})
     -- require('mini.git').setup({})
   end,
+
+  -- keys = {
+  --   {
+  --     "<leader>e",
+  --     function()
+  --       require("mini.files").open()
+  --     end,
+  --     desc = "File explorer",
+  --   },
+  -- },
 }
