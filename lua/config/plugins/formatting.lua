fd97be949251cbce27ecf1778de607d3b483f807
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2025-06-09 21:58:36 +0100

    use ruff for formatting and import sorting, add format toggle user command

--- lua/config/plugins/formatting.lua
+++ lua/config/plugins/formatting.lua
@@ -2,18 +2,47 @@ return {
   "stevearc/conform.nvim",
   -- lazy load from docs:
   -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#lazy-loading-with-lazynvim
-  event = { "BufWritePre" },
+  event = { "LspAttach", "BufWritePre" },
   cmd = { "ConformInfo" },
+  config = function(_, opts)
+    -- NOTE: if you use "config" and "opts" you need to do the setup manually
+    require("conform").setup(opts)
+
+    -- inspired from the below
+    -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+    vim.api.nvim_create_user_command("ToggleAutoFormat", function(args)
+      if args.bang then
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+        print("Buffer autoformat: " .. (vim.b.disable_autoformat and "OFF" or "ON"))
+      else
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+        print("Global autoformat: " .. (vim.g.disable_autoformat and "OFF" or "ON"))
+      end
+    end, {
+      desc = "Toggle autoformat-on-save (add ! for buffer-local)",
+      bang = true,
+    })
+  end,
   opts = {
     log_level = vim.log.levels.DEBUG,
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 2500,
-    },
+    format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 2500, lsp_format = "fallback" }
+    end,
     formatters_by_ft = {
       -- go = { 'gofmt' },
       -- rust = { 'rustfmt' },
-      python = { "black" },
+      python = {
+        -- To fix auto-fixable lint errors.
+        "ruff_fix",
+        -- To run the Ruff formatter.
+        "ruff_format",
+        -- To organize the imports.
+        "ruff_organize_imports",
+      },
       c = { "clang-format" },
       lua = { "stylua" },
       bash = { "shfmt", "shellcheck" },
