d8f9db1e54454ae0d4630a7fc94f128bf1fe72c2
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2026-01-31 15:37:37 +0000

    simplify colour scheme handling

--- lua/config/colorschemes.lua
+++ lua/config/colorschemes.lua
@@ -1,104 +1,41 @@
-local M = {}
-
--- TODO:
--- fix this and make it general so that it works with every colorscheme
-ToggleDarkMode = function()
-  local colorschemes = {
-    dark = "tokyonight-moon",
-    light = "tokyonight-day",
-  }
-  -- somehow when using vim.g.colors_name directly in the if statement it doesn't work
-  local current_colorscheme = vim.g.colors_name
-  if current_colorscheme == colorschemes.dark then
-    vim.cmd.colorscheme(colorschemes.light)
-  else
-    vim.cmd.colorscheme(colorschemes.dark)
-  end
-end
-
--- TODO:
--- add https://github.com/bluz71/vim-moonfly-colors
-M.colorscheme_conf = {
-  dracula = function()
-    vim.cmd.colorscheme("dracula")
-  end,
-  tokyonight = function()
-    vim.cmd.colorscheme("tokyonight-moon")
-    local opts = { noremap = true, silent = true }
-    vim.keymap.set("n", "<leader>tm", ToggleDarkMode, vim.tbl_extend("force", opts, { desc = "[t]oggle dark[m]ode" }))
-  end,
-  onedark = function()
-    require("onedark").setup({
-      style = "dark",
-    })
-    vim.cmd.colorscheme("onedark")
-  end,
-  gruvbox_material = function()
-    -- foreground option can be material, mix, or original
-    vim.g.gruvbox_material_foreground = "original"
-    --background option can be hard, medium, soft
-    vim.g.gruvbox_material_background = "hard"
-    vim.g.gruvbox_material_enable_italic = 1
-    vim.g.gruvbox_material_better_performance = 1
-    vim.cmd.colorscheme("gruvbox-material")
-  end,
-  -- nightfox = function()
-  --   vim.cmd([[colorscheme nordfox]])
-  -- end,
-  catppuccin = function()
-    -- available option: latte, frappe, macchiato, mocha
-    vim.g.catppuccin_flavour = "frappe"
-    require("catppuccin").setup()
-
-    vim.cmd.colorscheme("catppuccin")
-  end,
-  kanagawa = function()
-    vim.cmd("colorscheme kanagawa-wave")
-  end,
-  rose_pine = function()
-    vim.cmd("colorscheme rose-pine")
-  end,
-}
-
---- Use a random colorscheme from the pre-defined list of colorschemes.
-M.set_colorscheme = function(colorscheme)
-  if not vim.tbl_contains(vim.tbl_keys(M.colorscheme_conf), colorscheme) then
-    local msg = "Invalid colorscheme: " .. colorscheme
-    vim.notify(msg, vim.log.levels.ERROR, { title = "nvim-config" })
-    return
-  end
-
-  -- Load the colorscheme and its settings
-  M.colorscheme_conf[colorscheme]()
-
-  if vim.g.logging_level == "debug" then
-    local msg = "Colorscheme: " .. colorscheme
-    vim.notify(msg, vim.log.levels.DEBUG, { title = "nvim-config" })
-  end
-end
-
--- Load a random colorscheme
-
--- M.set_colorscheme("onedark")
-
 -- simplify colorscheme
 -- the below autocommand runs after the colorscheme is set
 -- makes the background a bit lighter than in the theme
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "lunaperche",
-  callback = function()
-    -- gray_bg is aligned with "backgroung" colour of Ghostty theme
-    local gray_bg = "#14161b"
-    local lighter_gray = "#23272f"
 
-    local set_hl = vim.api.nvim_set_hl
-    set_hl(0, "Normal", { bg = gray_bg })
-    set_hl(0, "NormalNC", { bg = gray_bg })
-    set_hl(0, "EndOfBuffer", { bg = gray_bg })
-    set_hl(0, "SignColumn", { bg = gray_bg })
-    set_hl(0, "CursorLine", { bg = lighter_gray })
-    set_hl(0, "NormalFloat", { bg = lighter_gray })
-  end,
-})
+local colorscheme = "default"
 
-vim.cmd.colorscheme("lunaperche")
+-- use the below as an example to change different highlight groups to adjust existing colorscheme/theme
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--   pattern = colorscheme,
+--   callback = function()
+--     -- gray_bg is aligned with "backgroung" colour of Ghostty theme
+--     local gray_bg = "#14161b"
+--     local lighter_gray = "#23272f"
+--
+--     local set_hl = vim.api.nvim_set_hl
+--
+--     local gray_bg_hg_groups = {
+--       "Normal",
+--       "NormalNC",
+--       "EndOfBuffer",
+--     }
+--     for _, hg_group in pairs(gray_bg_hg_groups) do
+--       set_hl(0, hg_group, { bg = gray_bg })
+--     end
+--
+--     local lighter_gray_hg_groups = {
+--       "CursorLine",
+--       "NormalFloat",
+--     }
+--     for _, hg_group in pairs(lighter_gray_hg_groups) do
+--       set_hl(0, hg_group, { bg = lighter_gray })
+--     end
+--
+--     set_hl(0, "VertSplit", {
+--       fg = "#141414",
+--       bg = "NONE",
+--     })
+--   end,
+-- })
+
+vim.cmd.colorscheme(colorscheme)
