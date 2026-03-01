08d7d1b064796baa9514b7bf5566593575a36c21
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2026-03-01 08:10:24 +0000

    turn off auto pairs

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -6,7 +6,7 @@ return {
     -- local statusline = require("mini.statusline")
     -- statusline.setup({ use_icons = true })
 
-    require("mini.pairs").setup({})
+    -- require("mini.pairs").setup({})
     require("mini.surround").setup({})
     -- require("mini.files").setup({})
     -- require('mini.git').setup({})
